368268048fbcf3f9f8f0bf73ed43c0eb1c237773
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-10-21 21:51:54 -0300

    finalizaçao

--- script.js
+++ script.js
@@ -6,11 +6,24 @@ function addTask(){
     if(taskText !== ""){
         const li = document.createElement("li");
         li.innerHTML = `
-        <span>${taskText}</span>
-        <button class="editButton" onClick="editTask()">Editar</button>
-        <button class="deleteButton" onClick="deleteTask()">Remover</button>
-        `
+            <span>${taskText}</span>
+            <button class="editButton" onclick="editTask(this)">Editar</button>
+            <button class="deleteButton" onclick="deleteTask(this)">Remover</button>
+        `;
         taskList.appendChild(li);
-        taskInput.value = ""
+        taskInput.value = "";
     }
+}
+
+function editTask(button){
+    const li = button.parentElement;
+    const span = li.querySelector("span");
+    const newText = prompt("Editar tarefa:", span.textContent);
+    if(newText !== null && newText.trim() !== ""){
+        span.textContent = newText.trim();
+    }
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
 }
